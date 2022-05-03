d6e702d070ef9cab703a4045e031c18c4e2f69a3
Cute Inspire <Randywahidin321@gmail.com>
2022-05-03 17:17:10 +0700

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -10,7 +10,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
                 callback_data=f"song_right B|{query_type}|{query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="Download",
+                text="𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝",
                 callback_data=f"qwertyuiopasdfghjkl {videoid}|{user_id}",
             ),
             InlineKeyboardButton(
@@ -20,7 +20,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Search",
+                text="𝐂𝐥𝐨𝐬𝐞",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -32,17 +32,17 @@ def song_download_markup(videoid, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="⬇️ Get Audio",
+                text="𝐆𝐞𝐭 𝐀𝐮𝐝𝐢𝐨",
                 callback_data=f"gets audio|{videoid}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="⬇️ Get Video",
+                text="𝐆𝐞𝐭 𝐕𝐢𝐝𝐞𝐨",
                 callback_data=f"gets video|{videoid}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Close Menu",
+                text="𝐂𝐥𝐨𝐬𝐞",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
